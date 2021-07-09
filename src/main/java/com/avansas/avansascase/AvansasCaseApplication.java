edf8122b2e76d643ceeb98dde4ab0282950c773c
furk2sahin <furkannsahin.7@gmail.com>
2021-07-09 20:48:20 +0300

    Initialize project

---
+++ src/main/java/com/avansas/avansascase/AvansasCaseApplication.java
@@ -0,0 +1,13 @@
+package com.avansas.avansascase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvansasCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvansasCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avansas/avansascase/AvansasCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avansas.avansascase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvansasCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
